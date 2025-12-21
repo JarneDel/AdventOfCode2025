900ac0ab2962aea821507a0fb416cf59914685f4
Jarne Delarue <jarne.delarue@numble.be>
2025-12-21 20:21:10 +0100

    fix

--- AdventOfCode.Solutions/SolutionResult.cs
+++ AdventOfCode.Solutions/SolutionResult.cs
@@ -4,6 +4,7 @@ public struct SolutionResult
 {
     public string Answer { get; set; }
     public TimeSpan Time { get; set; }
+    public long Memory { get; set; }
 
     public static SolutionResult Empty => new SolutionResult();
 }
